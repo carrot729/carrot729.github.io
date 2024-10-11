3618b7c97f7dc7b8b9bd6fc1cd455ece9e89e519
Caca Hitevision <hnf729@outlook.com>
2024-10-12 07:18:15 +0800

    12324

--- push.cmd
+++ push.cmd
@@ -1,8 +1,8 @@
 @echo off
-..\\..\\PortableGit\\bin\\git.exe add .
+git.exe add .
 set /p commit="add commit:"
-..\\..\\PortableGit\\bin\\git.exe commit -m "%commit%"
+git.exe commit -m "%commit%"
 echo pushing......
-..\\..\\PortableGit\\bin\\git.exe push origin main
+git.exe push origin main
 echo done
 pause
